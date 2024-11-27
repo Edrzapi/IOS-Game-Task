c6c156ba4a31d21afeec65093eb59e4969f44d55
edrzapi <edrzapi@gmail.com>
2024-11-27 14:50:46 +0000

    upload ans

--- IOS-Tapper-Game/GameModel.swift
+++ IOS-Tapper-Game/GameModel.swift
@@ -9,14 +9,24 @@ class GameModel: ObservableObject {
 
     // Start the game
     func startGame() {
-    
-        
+        score = 0
+        timeRemaining = 10
+        isGameActive = true
+	
         timer?.invalidate()  // Reset any existing timer
         timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            self.timeRemaining -= 1
+            if self.timeRemaining <= 0 {
+                self.timer?.invalidate()
+                self.isGameActive = false  // Stop the game when time runs out
+            }
         }
+    }
 
     // Increment the score when the button is tapped
     func buttonTapped() {
-       
+        if isGameActive {
+            score += 1
+        }
     }
 }
